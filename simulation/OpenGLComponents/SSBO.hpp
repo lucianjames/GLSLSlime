8e4f072d529b3f20e51d03b3aed0863e0f4c8a56
LJ3D <65134690+LJ3D@users.noreply.github.com>
2023-05-21 18:06:45 +0100

    Delete random commented out code that i forgot about

--- simulation/OpenGLComponents/SSBO.hpp
+++ simulation/OpenGLComponents/SSBO.hpp
@@ -41,26 +41,3 @@ namespace openGLComponents{
 
     };
 }
-
-
-/*
-
-SSBO::SSBO(const void* data, unsigned int size){
-	GLCall(glGenBuffers(1, &ID));
-	GLCall(glBindBuffer(GL_SHADER_STORAGE_BUFFER, ID));
-	GLCall(glBufferData(GL_SHADER_STORAGE_BUFFER, size, data, GL_DYNAMIC_COPY));	
-}
-
-SSBO::~SSBO(){
-	GLCall(glDeleteBuffers(1, &ID));
-}
-
-void SSBO::bind(unsigned int shaderID, const char name[]) const{
-	unsigned int block_index;
-	block_index = glGetProgramResourceIndex(shaderID, GL_SHADER_STORAGE_BLOCK, name);
-	glShaderStorageBlockBinding(shaderID, block_index, 0);
-	unsigned int binding_point_index = 0;
-	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, binding_point_index, ID);
-}
-
-*/
